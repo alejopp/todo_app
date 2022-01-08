7eee835c54f65eed1335b3461f6e76231b9b433a
yapelaezp <yapelaezp@unal.edu.co>
2022-01-07 20:51:41 -0500

    Form to add todos added

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,15 +1,46 @@
 import React from "react";
+import { TodoContext } from "../TodoContext";
+import  "./TodoForm.css";
 
 function TodoForm(){
+    const [newTodoValue, setNewTodoValue] = React.useState('');
+    const { addTodo,setOpenModal } = React.useContext(TodoContext);
+
+    const onCancel = () => {
+        setOpenModal(false);
+    }
+
+    const onChange = (event) => {
+        setNewTodoValue(event.target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addTodo(newTodoValue); 
+        setOpenModal(false);
+    }
 
     return(
-        <form>
+        <form onSubmit={onSubmit}>
             <label>
 
             </label>
+            <textarea
+                placeholder="e.g read a book"
+                onChange={onChange}
+                value={newTodoValue}
+            />
+            <div>
+                <button type= "button"  onClick={onCancel} >
+                    Cancel
+                </button>
+                <button type= "submit" >
+                    Add Todo
+                </button>
+            </div>
        </form>
     );
 }
 
 
-export {TodoForm};
+export { TodoForm };
